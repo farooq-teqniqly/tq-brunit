944ef5c94c55d8865ff71c8be37849acd678b418
farooq-teqniqly <farooq@teqniqly.com>
2025-12-29 19:52:10 -0800

    ci: Add class and test to get github workflow running

---
+++ Teqniqly.BRUnit.Testing/Calculator.cs
@@ -0,0 +1,10 @@
+namespace Teqniqly.BRUnit.Testing;
+
+/// <summary>
+/// A simple calculator class. This class is just used to get the CI/CD pipeline running.
+/// It will be removed once the real code is in place.
+/// </summary>
+public static class Calculator
+{
+    public static int Add(int a, int b) => a + b;
+}
